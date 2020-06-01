66567e0abef284aa6e933a27f32f68a6d36d2845
GuoBin <guobin201314@gmail.com>
2020-06-01 15:47:56 +0800

    style(app): loading
    before app start loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+/**
+ * 静态loading
+ */
+export const Loading = (
+  <div className="center">
+    <svg
+      className="m-loading"
+      xmlns="http://www.w3.org/2000/svg"
+      width={42}
+      height={42}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="10" />
+    </svg>
+  </div>
+)
--- src/index.tsx
+++ src/index.tsx
@@ -4,30 +4,40 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 import './index.less'
 import 'antd/dist/antd.less'
+import { appService } from './core/app-service'
+import { Loading } from './components/Loading'
 
 const Admin = React.lazy(() => import('./routes/AdminRoute'))
 const Login = React.lazy(() => import('./routes/LoginRoute'))
 const Visitor = React.lazy(() => import('./routes/VisitorRoute'))
 
-ReactDOM.render(
-  <Suspense fallback={<div>loading</div>}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/visitor">
-          <Visitor />
-        </Route>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-        <Route path="">
-          <Login />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  </Suspense>,
-  document.getElementById('root')
-)
+ReactDOM.render(Loading, document.getElementById('root'))
+
+appService
+  .bootstrap()
+  .then(() => {
+    ReactDOM.render(
+      <Suspense fallback={Loading}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/visitor">
+              <Visitor />
+            </Route>
+            <Route path="/admin">
+              <Admin />
+            </Route>
+            <Route path="">
+              <Login />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </Suspense>,
+      document.getElementById('root')
+    )
+  })
+  .catch()
+
 serviceWorker.unregister()
