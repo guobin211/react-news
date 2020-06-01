65752029adad3afcca2348557e87cd24198a6d10
GuoBin <guobin201314@gmail.com>
2020-06-01 15:48:27 +0800

    feature(components): ProgressBar
    进度条组件

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export interface ProgressBarProps {
+  value: number
+  showValue: boolean
+  unit: number
+  mode: string
+}
+
+export interface ProgressBarState {}
+
+/**
+ * 进度条组件
+ */
+export default class ProgressBar extends React.Component<ProgressBarProps, ProgressBarState> {
+  render() {
+    return <div className="ProgressBar">HELLO</div>
+  }
+}
