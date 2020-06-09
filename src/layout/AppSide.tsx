c26c6cfca07c5741e7b238c1f507f7c06473e6d5
GuoBin <guobin201314@gmail.com>
2020-06-09 15:48:04 +0800

    fix(AppSide): Menu.Item key use path

--- src/layout/AppSide.tsx
+++ src/layout/AppSide.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Layout, Menu } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
 import store from '../store'
-import { RouteNav } from '../store/reducers/routes-reducer'
+import { RouteNav } from '@/store/reducers/routes-reducer'
 
 const { SubMenu } = Menu
 
@@ -19,23 +19,22 @@ export default class AppSide extends React.Component<AppSideProps, AppSideState>
   state = {
     routes: store.getState().routesState
   }
-  defaultSelectedKeys = ['/admin/dashboard/analysis0']
-
+  defaultSelectedKeys = ['/admin/dashboard/analysis']
   side: CSSProperties = {
     minHeight: '100vh'
   }
 
-  renderMenu(route: RouteNav, key: number) {
+  renderMenu(route: RouteNav) {
     const icon = route.icon ? route.icon : <UserOutlined />
     if (route.routes && route.routes.length > 0) {
       return (
-        <SubMenu key={route.path + key} icon={icon} title={route.desc}>
-          {route.routes.map((route, index) => this.renderMenu(route, index))}
+        <SubMenu key={route.path} icon={icon} title={route.desc}>
+          {route.routes.map((route, index) => this.renderMenu(route))}
         </SubMenu>
       )
     } else {
       return (
-        <Menu.Item key={route.path + key} icon={icon}>
+        <Menu.Item key={route.path} icon={icon}>
           <Link to={route.path}>{route.desc}</Link>
         </Menu.Item>
       )
@@ -47,7 +46,7 @@ export default class AppSide extends React.Component<AppSideProps, AppSideState>
       <Layout.Sider trigger={null} collapsible collapsed={this.props.collapsed} style={this.side}>
         <div className="logo" />
         <Menu theme="dark" mode="inline" defaultSelectedKeys={this.defaultSelectedKeys}>
-          {this.state.routes.map((route, index) => this.renderMenu(route, index))}
+          {this.state.routes.map((route) => this.renderMenu(route))}
         </Menu>
       </Layout.Sider>
     )
