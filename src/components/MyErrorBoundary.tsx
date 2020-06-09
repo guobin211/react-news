bf4933c56ea5927d9d054a543e48f90932b44137
GuoBin <guobin201314@gmail.com>
2020-06-09 15:49:40 +0800

    feature(Hooks): hooks
    使用hooks简化代码

---
+++ src/components/MyErrorBoundary.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export interface MyErrorBoundaryProps {}
+
+export interface MyErrorBoundaryState {
+  hasError: boolean
+}
+
+export default class MyErrorBoundary extends React.Component<MyErrorBoundaryProps, MyErrorBoundaryState> {
+  constructor(props: MyErrorBoundaryProps) {
+    super(props)
+    this.state = {
+      hasError: false
+    }
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.log(error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // 你可以自定义降级后的 UI 并渲染
+      return <h1>Something went wrong.</h1>
+    }
+    return this.props.children
+  }
+}
---
+++ src/components/MyHooks.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+const MyHooks = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  }, [count]) // 仅在 count 更改时更新
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
+}
+export default MyHooks
