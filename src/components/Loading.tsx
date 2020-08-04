55c2178366d16d2c750f3d240085ff54fe1098d9
GuoBin <guobin201314@gmail.com>
2020-08-04 14:17:38 +0800

    feat(loading): loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,13 +4,14 @@ import React from "react"
  * 静态loading
  */
 export const Loading: React.FC = (props: any) => {
+  const { width, height } = props
   return (
     <div className="center">
       <svg
         className="m-loading"
         xmlns="http://www.w3.org/2000/svg"
-        width={42}
-        height={42}
+        width={width || 42}
+        height={height || 42}
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
