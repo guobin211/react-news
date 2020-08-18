ed495a7545e7b11029e932cde4ba20405a1a1ffb
GuoBin <guobin201314@gmail.com>
2020-08-18 18:01:40 +0800

    feat(form): form input

--- src/pages/form/BasicFormPage.tsx
+++ src/pages/form/BasicFormPage.tsx
@@ -1,11 +1,28 @@
-import React from "react"
+import React, { ChangeEvent } from "react"
 
 export interface BasicFormPageProps {}
 
-export interface BasicFormPageState {}
+export interface BasicFormPageState {
+  value: string
+}
 
 export default class BasicFormPage extends React.Component<BasicFormPageProps, BasicFormPageState> {
+  state = {
+    value: ""
+  }
+
+  handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: ev.target.value }, () => {
+      console.log("change event")
+    })
+  }
+
   render() {
-    return <div className="BasicFormPage">BasicFormPage</div>
+    return (
+      <div className="BasicFormPage">
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <p>{this.state.value}</p>
+      </div>
+    )
   }
 }
--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,22 +1,39 @@
 import React from "react"
+import Loadable from "react-loadable"
+import { LoaderConfig } from "@/pages/LoaderConfig"
 import { RouteConfig } from "@/routes"
+
+const BasicFormPage = Loadable({
+  loader: () => import("./BasicFormPage"),
+  ...LoaderConfig
+})
+
+const AdvancedFormPage = Loadable({
+  loader: () => import("./AdvancedFormPage"),
+  ...LoaderConfig
+})
+
+const StepsPage = Loadable({
+  loader: () => import("./StepFormPage"),
+  ...LoaderConfig
+})
 
 export const Form: RouteConfig[] = [
   {
     path: "/form/basic",
-    component: React.lazy(() => import("./BasicFormPage")),
+    component: BasicFormPage,
     desc: "基础表单",
     level: 3
   },
   {
     path: "/form/advanced",
-    component: React.lazy(() => import("./AdvancedFormPage")),
+    component: AdvancedFormPage,
     desc: "高级表单",
     level: 3
   },
   {
     path: "/form/step",
-    component: React.lazy(() => import("./StepFormPage")),
+    component: StepsPage,
     desc: "分步表单",
     level: 3
   }
